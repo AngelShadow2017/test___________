11f2a393bde0146bcc904c0db25a16159045f2e5
泠漪 <1509095749@qq.com>
2022-01-09 10:36:13 +0800

    updated 1/9

--- en-us/Custom_Modifications.js
+++ en-us/Custom_Modifications.js
@@ -1,6 +1,11 @@
 "use strict";
 {
-    let FilePath = "./languages/UI_Language/en-us/"
+    let FilePath = "./languages/UI_Language/en-us/";
+    let buttonPath = './uis/Buttons/';
+    let buttons = ["Start","Cancel","Continue","Menu","Mirror","OK","Reset","Speed"];
+    for(let i in buttons){
+        buttons[i] = FilePath+buttonPath+buttons[i]+".webp";
+    }
     loadRes({
         "js":[],
         "css":[],
@@ -8,8 +13,8 @@
             FilePath+"uis/main.webp",
             FilePath+"uis/map.webp",
             FilePath+"uis/Fuben1.webp",
-            FilePath+"uis/Introduction.webp"
-        ],
+            FilePath+"uis/Introduction.webp",
+        ].concat(buttons),
         "au":[]
     });
     jngTemplate.template.Writer = `<div id="wrap"><img src="${FilePath}uis/writers.webp"></div>`;
