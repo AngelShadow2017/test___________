2f0fd507d6989d80fd02bf975b3f9492909bff1f
泠漪 <1509095749@qq.com>
2022-04-05 16:32:23 +0800

    Update Custom_Modifications.js

--- en-us/Custom_Modifications.js
+++ en-us/Custom_Modifications.js
@@ -21,4 +21,28 @@
     });
     jngTemplate.template.Writer = `<div id="wrap"><img src="${FilePath}uis/writers.webp"></div>`;
     
+    {
+        let shakeText = oEffects.TextEffects.ShakeText;
+        oEffects.TextEffects.ShakeText = function(json){
+            if(!json.customSize){
+                json.customSize = function(i){
+                    let size = -1;
+                    let sizes = [1025,938,825+125,1025,900,775,1025,950,550,750,1012,850,1225,1025,1100,900,1100,987,737,900,1000,1172,1300,1087,1037,887];
+                    let sizes_lower = [1025,938,825+125,1025,900,775,1025,950,550,750,1012,850,1225,1025,1100,900,1100,987,737,900,1000,1172,1300,1087,1037,887];
+                    if(i>='A'&&i<='Z'){
+                        size = sizes[i.charCodeAt()-65]/2000;
+                    }
+                    if(i>="a"&&i<='z'){
+                        size = sizes_lower[i.charCodeAt()-97]/2000;
+                    }
+                    if(i=='1'){
+                        size=0.4;
+                    }
+                    return size;
+                };
+            }
+            return shakeText.bind(oEffects.TextEffects)(json);
+        };
+        console.log(oEffects.TextEffects.ShakeText);
+    }
 };
