44c4ad6d311ae0cbc0f1b9f927f71c21ac9703a2
泠漪 <32807696+AngelShadow2017@users.noreply.github.com>
2022-10-04 23:24:21 +0800

    uploaded

--- en-us/Custom_Modifications.js
+++ en-us/Custom_Modifications.js
@@ -1,10 +1,8 @@
 "use strict";
 {
     let FilePath = "./languages/UI_Language/en-us/";
-    loadRes({
-        "js":[],
-        "css":[],
-        "img":[
+    oLoadRes.loadImage({
+        resourceArr: [
             FilePath+"uis/main.webp",
             FilePath+"uis/map.webp",
             FilePath+"uis/Fuben1.webp",
@@ -13,7 +11,6 @@
             FilePath+"uis/Almanac.webp",
             FilePath+"uis/GameOver.png",
         ],
-        "au":[]
     });
     jngTemplate.template.Writer = `<div id="wrap"><img src="${FilePath}uis/writers.webp"></div>`;
     jngTemplate.template.Diary = `<iframe id='DiaryIframe' class="NoBar" width="900" height="600" frameBorder='0' scrolling='auto' src='frame/Diary.html?folder=../${FilePath}/uis/diary/'></iframe>`;
@@ -35,29 +32,4 @@
             <a class="jngButton ArrowMap ArrowLast" onclick="labMap.change(-1)"></a>
         </div>
         `;
-    /*{
-        let shakeText = oEffects.TextEffects.ShakeText;
-        oEffects.TextEffects.ShakeText = function(json){
-            if(!json.customSize){
-                json.customSize = function(i){
-                    let size = -1;
-                    let sizes = [1025,938,1050,1025,900,910,1125,900,750,790,1012,1100,1225,1125,1100,1050,1100,920,900,1000,1050,1370,1350,1070,1037,900];
-                    let sizes_lower = [985,930,965,1005,950,800,1040,830,700,800,750,1020,1250,1025,1090,1000,1000,800,800,880,1010,1050,1130,970,950,887];
-                    let arrNum = [1050,880,965,980,960,970,920,980,1050,1030];
-                    if(i>="0"&&i<="9"){
-                        size = arrNum[i.charCodeAt()-48]/2000;
-                    }
-                    if(i>='A'&&i<='Z'){
-                        size = sizes[i.charCodeAt()-65]/2000;
-                    }
-                    if(i>="a"&&i<='z'){
-                        size = sizes_lower[i.charCodeAt()-97]/2000;
-                    }
-                    return size;
-                };
-            }
-            return shakeText.bind(oEffects.TextEffects)(json);
-        };
-        console.log(oEffects.TextEffects.ShakeText);
-    }*/
 };
